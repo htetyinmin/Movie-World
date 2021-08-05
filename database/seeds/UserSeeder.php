8b487b2061027e275503a0aec351b958504524bf
KyawWinTun-vix <vixenhello1998@gmail.com>
2021-08-05 22:37:34 +0630

    something

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Traits\HasRoles;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
